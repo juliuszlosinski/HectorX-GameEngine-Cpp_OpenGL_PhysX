384050ae0f29c0a95ef449de5b071c48ddc84d8f
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2021-08-26 17:24:46 +0200

    1

--- repos/OpenGL_App/OpenGL_App/main.cpp
+++ repos/OpenGL_App/OpenGL_App/main.cpp
@@ -98,7 +98,7 @@ int main(void)
 	CreateShaders();
 
 	/// * Inicjalizacja kamery.
-	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 5.0f, 0.5f);
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 5.0f, 0.25f);
 
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
 
